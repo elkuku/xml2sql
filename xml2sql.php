24176b794b1484236633b965cd98ae549bbd38d2
Nikolai Plath <der.el.kuku@gmail.com>
2011-12-26 03:40:43 -0500

    Add --createall

--- xml2sql.php
+++ xml2sql.php
@@ -12,7 +12,6 @@ ini_set('error_reporting', E_ALL | E_STRICT);
  * Bootstrap the Joomla! Platform.
  */
 require $_SERVER['JOOMLA_PLATFORM_PATH'].'/libraries/import.php';
-// require '/home/elkuku/eclipsespace/indigogit3/joomla-platform-testing/libraries/import.php';
 
 define('JPATH_BASE', dirname(__FILE__));
 define('JPATH_SITE', JPATH_BASE);
@@ -55,9 +54,46 @@ class XML2SQL extends JCli
         $this->out('|-------------------------|');
         $this->out();
 
+        if($this->input->get('create'))
+        {
+            $this->create();
+            $input = 'xml2sql-created.xml';
+            $output = 'xml2sql-created.'.$format.'.sql';
+        }
+
+        if($this->input->get('createall'))
+        {
+            $this->create();
+
+            $input = 'xml2sql-created.xml';
+
+            foreach (new DirectoryIterator(JPATH_BASE.'/formats') as $fInfo)
+            {
+                if($fInfo->isDot())
+                continue;
+
+                $format = $fInfo->getBasename('.php');
+
+                $output = 'xml2sql-created.'.$format.'.sql';
+
+                $this->createSql($input, $output, $format);
+            }
+        }
+
+
+
+        $this->out('Finished =;)');
+        $this->out();
+    }//function
+
+    private function createSql($input, $output, $format)
+    {
+
         if( ! $input || ! $output || ! $format)
         throw new Exception('Missing values. Usage: -i <inputfile> -o <outputfile> --format <format>', 1);
 
+        $this->out();
+        $this->out('Format: '.$format);
         $this->out('Input:  '.$input);
         $this->out('Output: '.$output);
         $this->out();
@@ -88,25 +124,115 @@ class XML2SQL extends JCli
         throw new Exception('Invalid xml file: '.$input, 1);
 
         $sql = '';
+        $cnt = 0;
 
         //-- Process "CREATE TABLE" stanetments
         foreach ($xml->database->table_structure as $create)
         {
             $sql .= $this->formatter->formatCreate($create);
+            $cnt ++;
         }//foreach
+
+        $this->out(sprintf('Processed %d create queries.', $cnt));
+
+        $cnt = 0;
 
         //-- Process "INSERT" statements
         foreach ($xml->database->table_data as $insert)
         {
             $sql .= $this->formatter->formatInsert($insert);
+            $cnt ++;
         }//foreach
+
+        $this->out(sprintf('Processed %d insert queries.', $cnt));
 
         if( ! JFile::write($output, $sql))
         throw new Exception('Unable to generate the output file at: '.$output, 1);
 
-        $this->out('Finished =;)');
-        $this->out();
-    }//function
+        $this->out(sprintf('Output file created at: %s', $output));
+    }
+
+    private function create()
+    {
+        $config = JFactory::getConfig();
+
+        $jBase = $config->get('jbase');
+
+        $this->out('JBase:'.$jBase);
+
+        if( ! is_dir($jBase))
+        throw new Exception('Invalid $jbase set in configuration.php');
+
+        if( ! is_dir($jBase.'/installation'))
+        throw new Exception('No installation dir found in specified $jbase dir set in configuration.php');
+
+        $this->out('Create db...', false);
+
+        $options = array(
+            'driver' => $config->get('dbtype', 'mysqli'),
+            'host' => $config->get('host'),
+            'user' => $config->get('user'),
+            'password' => $config->get('password'),
+            'database' => $config->get('db'),
+            'prefix' => $config->get('dbprefix'),
+            'select' => false,
+        );
+
+        $db = JDatabase::getInstance($options);
+
+        $query = $db->getQuery(true);
+
+        $db->setQuery('CREATE DATABASE '.$config->get('db'))->query();
+        $db->setQuery('USE '.$config->get('db'))->query();
+
+        $this->out('ok');
+
+        $this->out('Fill db...', false);
+
+        $sql = JFile::read($jBase.'/installation/sql/mysql/joomla.sql');
+
+        $queries = $db->splitSql($sql);
+
+        $this->out(sprintf('Found %d queries...', count($queries)), false);
+
+        foreach ($queries as $i => $query)
+        {
+            if($i && $i / 10 == floor($i / 10))
+            $this->out($i.'...', false);
+
+            $db->setQuery($query)->query();
+        }
+
+        $this->out(($i + 1).'...', false);
+
+        $this->out('ok');
+
+        $this->out('dump db to XML...', false);
+
+        $connData = '';
+
+        $connData .= ' -u '.$config->get('user');
+        $connData .= ' -h '.$config->get('host');
+
+        if($config->get('password'))
+        $connData .= ' -p '.$config->get('password');
+
+        $connection = $config->get('mysqlpath').'/mysqldump --xml '.$connData;
+
+        $cmd = $connection
+        .' '.$config->get('db')
+        .' > xml2sql-created.xml';
+
+        echo shell_exec($cmd);
+
+        $this->out('ok');
+
+        $this->out('delete db...', false);
+
+        $db->setQuery('DROP DATABASE '.$config->get('db'))->query();
+
+        $this->out('ok');
+    }
 
 }//class
 
